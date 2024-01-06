8d9d7ca4320018eb4e56c545b7be31db9f7f1eca
Carmen Ortigoza <43401320+tifansun@users.noreply.github.com>
2024-01-05 20:35:34 -0600

    recursos adicionales

--- sketch.js
+++ sketch.js
@@ -1,18 +1,12 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
 function preload() {
-
+  
 }
 
 function setup() {
   canvas = createCanvas(1200, 600);
-  motor = Engine.create();
-  mundo = motor.world;
+  
 }
 
 function draw() {
-  Engine.update(motor);
+
 }
